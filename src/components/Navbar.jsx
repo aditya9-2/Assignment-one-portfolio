3269ed5901d476064717b4d9cfe82069649579f2
Aditya <adityabasak7031@gmail.com>
2024-08-18 17:31:19 +0530

    remove extra white space

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,3 @@
-
-// import logo from "../assets/abLogo.png";
-// // use Recat-icons for icons
-// import { FaLinkedin, FaGithub } from "react-icons/fa";
-// import { FaXTwitter } from "react-icons/fa6";
-// const Navbar = () => {
-//     return <nav className="mb-20 flex items-center justify-between py-6">
-
-//         <div className="flex flex-shrink-0 items-center">
-//             <img src={logo} alt="LOGO" className="mx-2 w-10" />
-//         </div>
-
-//         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-//             <FaLinkedin />
-//             <FaGithub />
-//             <FaXTwitter />
-//         </div>
-
-//     </nav>
-// }
-
-// export default Navbar
-
-
 import logo from "../assets/abLogo.png";
 // Use React-icons for icons
 import { FaLinkedin, FaGithub } from "react-icons/fa";
