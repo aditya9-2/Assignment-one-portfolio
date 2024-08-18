6fd77f70cfd325f51ff22130a58d2af7c20e0584
Aditya <adityabasak7031@gmail.com>
2024-08-18 17:17:03 +0530

    complete button section on the projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,13 +12,15 @@ const Projects = () => {
             description:
                 "GOLOC, a React e-commerce app, uses Strapi API and Stripe payment gateway. GOLOC sources products from local vendors when markets have no supply and charges extra. This sets GOLOC apart from other location-based apps.",
             technologies: ["HTML", "CSS", "React", "Node.js", "Strapi"],
+            link: "https://github.com/aditya9-2/GOLOC"
         },
         {
-            title: "Task Management App",
+            title: "Healthzoid - Muliple disease Detection System",
             image: healthzoid,
             description:
                 "It's an app covering an area to detect some common and yet diseases full of risks so that a person can be able to find the symptoms without the visit to the physician. These are the diseases: brain tumour; breast cancer; parkinson's disease; heart disease ; COVID 19 ; diabetes ; pneumonia and alzheimer's.",
             technologies: ["HTML", "CSS", "Python", "Anaconda", "Jupyter Notebook"],
+            link: "https://github.com/aditya9-2/Healthzoid"
         },
         {
             title: "Portfolio Website",
@@ -26,6 +28,7 @@ const Projects = () => {
             description:
                 "A personal portfolio website showcasing projects, skills, and contact information.",
             technologies: ["HTML", "CSS", "React", "Vite"],
+            link: "https://google.com" // Link to change after deploy
         },
         {
             title: "Face Recognition Attendance System",
@@ -33,6 +36,7 @@ const Projects = () => {
             description:
                 "A platform for easing the work for many offices where an employee or a student can give their attendance.",
             technologies: ["Python"],
+            link: "https://github.com/aditya9-2/python-basic-to-intermediate-five-projects/tree/master/faceRecognitionAttendanceSystem",
         },
     ];
 
@@ -54,16 +58,26 @@ const Projects = () => {
                         <div className="w-full max-w-xl lg:w-3/4">
                             <h6 className="mb-2 font-semibold">{project.title}</h6>
                             <p className="mb-4 text-neutral-400">{project.description}</p>
-                            <div>
+                            <div className="mb-4">
                                 {project.technologies.map((tech, index) => (
                                     <span
                                         key={index}
-                                        className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900"
+                                        className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
                                     >
                                         {tech}
                                     </span>
                                 ))}
                             </div>
+                            <a
+                                href={project.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+                            >
+                                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                    View Project
+                                </span>
+                            </a>
                         </div>
                     </div>
                 ))}
