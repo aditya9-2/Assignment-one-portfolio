86734bc5df549e96bae93a2a5fa7cc833efaa0bc
Aditya <adityabasak7031@gmail.com>
2024-08-19 20:38:21 +0530

    complete Header Animation-About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,14 @@ const About = () => {
     const aboutPara = `I am a dedicated and versatile Web developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
     return (
         <div className="border-b border-neutral-900 pb-4">
-            <h2 className="my-20 text-center text-4xl">About <span className="text-neutral-500">Me</span></h2>
+
+            <motion.h2
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 1.5 }}
+                className="my-20 text-center text-4xl"
+            >About <span className="text-neutral-500">Me</span>
+            </motion.h2>
 
             <div className="flex flex-wrap">
 
