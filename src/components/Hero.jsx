c61ac9a1642d5b486afe7d4ce580a4f664c3cf3e
Aditya <adityabasak7031@gmail.com>
2024-08-19 21:08:39 +0530

    fixed:correct file name and path for resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -53,8 +53,8 @@ const Hero = () => {
                             variants={container(1.2)}
                             initial="hidden"
                             animate="visible"
-                            href="/resume.pdf"
-                            download="Aditya_Basak_Resume.pdf"
+                            href="/Aditya-Resume.pdf"
+                            download="Aditya-Resume.pdf"
 
                             className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white  dark:focus:ring-blue-800">
                             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
